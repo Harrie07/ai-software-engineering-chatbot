97aa335922e6e091e196f108165a2f371b90435b
Harshal Sakpal <harshalsakpal21@gmail.com>
2024-12-10 18:23:49 +0530

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
+# Store your API key here
+API_KEY = "AIzaSyCuc2hf799ivrRe7jVr2Jq28C_YjU5a4Bs"
+# GOOGLE_API_KEY = "AIzaSyCuc2hf799ivrRe7jVr2Jq28C_YjU5a4Bs"
 
+
+# Function to get the API key
+def get_model():
+    return API_KEY
